0d5b48a3b0c107cf09f0813d5bc67b2cc1496a05
Subak Systems <info@subak.jp>
2013-05-10 18:52:33 +0900

    2013年 5月10日 金曜日 18時52分33秒 JST

--- domino-class.js
+++ domino-class.js
@@ -2,7 +2,7 @@ function define(callback) {
   module.exports = callback();
 }
 var fs = require('fs'),
-  callback = eval(fs.readFileSync("./lib/class.js", 'utf8'));
+  callback = eval(fs.readFileSync(__dirname + "/lib/class.js", 'utf8'));
 
 var Class = module.exports;
 
